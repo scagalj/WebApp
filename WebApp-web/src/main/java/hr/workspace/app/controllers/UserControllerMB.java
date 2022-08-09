dd344a87477814ace57336b9d060baab38ab406a
Stjepan <Stjepan@DESKTOP-HPJAV2O>
2022-08-09 16:02:30 +0200

    added documents

--- WebApp-web/src/main/java/hr/workspace/app/controllers/UserControllerMB.java
+++ WebApp-web/src/main/java/hr/workspace/app/controllers/UserControllerMB.java
@@ -6,6 +6,7 @@
 package hr.workspace.app.controllers;
 
 import hr.workspace.controllers.interfaces.UserController;
+import hr.workspace.models.Attachment;
 import hr.workspace.models.ContactUser;
 import hr.workspace.models.Representative;
 import javax.ejb.EJB;
@@ -15,6 +16,8 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import org.primefaces.event.RowEditEvent;
+import org.primefaces.model.file.UploadedFile;
+import org.primefaces.model.file.UploadedFiles;
 
 /**
  *
@@ -28,6 +31,7 @@ public class UserControllerMB extends BaseManagedBean{
     private UserController controller;
     private Representative representative;
     private ContactUser contact;
+    private UploadedFiles attachments;
 
     public void saveUser(){
         setUser(controller.saveUser(getSecurityContext(), getUser()));
@@ -94,5 +98,34 @@ public class UserControllerMB extends BaseManagedBean{
     public void setContact(ContactUser contact) {
         this.contact = contact;
     }
+    
+    public void uploadMultiple() {
+        if (getAttachments() != null) {
+            for (UploadedFile f : getAttachments().getFiles()) {
+                controller.saveAttachmen(getSecurityContext(),getUser(), f);
+                System.out.println("FILE: " + f.getFileName());
+            }
+            setAttachments(null);
+        }
+    }
+    
+    public void deleteAttachment(Attachment att){
+        Boolean deleteAttachment = controller.deleteAttachment(getSecurityContext(), getUser(), att);
+        if(deleteAttachment){
+            System.out.println("Uspijesno izbrisano.");
+        }else{
+            System.out.println("NIje uspio izbrisati attachment.");
+        }
+    }
+
+    public UploadedFiles getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(UploadedFiles attachments) {
+        this.attachments = attachments;
+    }
+    
+    
     
 }
