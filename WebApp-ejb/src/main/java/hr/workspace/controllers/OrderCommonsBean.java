30cc920f23d07eeb6b2325405c6b6b10666245e8
Stjepan <Stjepan@DESKTOP-HPJAV2O>
2022-10-01 14:04:28 +0200

    fix new order

--- WebApp-ejb/src/main/java/hr/workspace/controllers/OrderCommonsBean.java
+++ WebApp-ejb/src/main/java/hr/workspace/controllers/OrderCommonsBean.java
@@ -30,8 +30,8 @@ import javax.persistence.Query;
  */
 @Stateless
 @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
-public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements OrderCommons{
-    
+public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements OrderCommons {
+
     @Override
     public List<UserOrder> getAll(SecurityContext sc) {
         try {
@@ -42,7 +42,7 @@ public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements 
         }
         return null;
     }
-    
+
     @Override
     public List<UserOrder> getAllActive(SecurityContext sc) {
         try {
@@ -53,10 +53,9 @@ public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements 
         }
         return null;
     }
-    
-    
+
     @Override
-    public List<UserOrder> getCurrentlyActiveOrderForUser(SecurityContext sc, ContactUser user, SalesObject salesObject){
+    public List<UserOrder> getCurrentlyActiveOrderForUser(SecurityContext sc, ContactUser user, SalesObject salesObject) {
         System.out.println("TEST1");
         Query query = em.createQuery("Select u from UserOrder u "
                 + "where u.contactUser = :contactUser "
@@ -69,11 +68,11 @@ public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements 
 //        Query q = em.createNativeQuery("Select u From UserOrder u where u.contactuser_id = #id and u.salesobject_id = #salesObjectId and (userorderstatus = 0 or userorderstatus = 1)");
         List<UserOrder> orders = new ArrayList<>(query.getResultList());
         return orders;
-        
+
     }
-    
+
     @Override
-    public List<UserOrder> getCompletedOrdersForSalesObject(SecurityContext sc, SalesObject salesObject){
+    public List<UserOrder> getCompletedOrdersForSalesObject(SecurityContext sc, SalesObject salesObject) {
         Query query = em.createQuery("Select u from UserOrder u "
                 + "where u.salesObject = :salesObject "
                 + "and (u.userOrderStatus = :init or u.userOrderStatus = :progress)");
@@ -83,9 +82,9 @@ public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements 
         List<UserOrder> orders = new ArrayList<>(query.getResultList());
         return orders;
     }
-    
+
     @Override
-    public List<UserOrder> getAllOrdersForSalesObjectForUser(SecurityContext sc,ContactUser user, SalesObject salesObject){
+    public List<UserOrder> getAllOrdersForSalesObjectForUser(SecurityContext sc, ContactUser user, SalesObject salesObject) {
         Query query = em.createQuery("Select u from UserOrder u "
                 + "where u.salesObject = :salesObject "
                 + "and u.contactUser = :contactUser");
@@ -94,15 +93,15 @@ public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements 
         List<UserOrder> orders = new ArrayList<>(query.getResultList());
         return orders;
     }
-    
+
     @Override
-    public Map<Product,Integer> calculateAvailabeQuantityPerProduct(SecurityContext sc, List<Product> products, List<UserOrder> orders){
-        Map<Product,Integer> result = new HashMap<>();
-        List<OrderItem> orderItems  = orders.stream().flatMap(o -> o.getOrderItems().stream()).collect(Collectors.toList());
-        for(OrderItem oi : orderItems){
-            if(result.containsKey(oi.getProduct())){
+    public Map<Product, Integer> calculateAvailabeQuantityPerProduct(SecurityContext sc, List<Product> products, List<UserOrder> orders) {
+        Map<Product, Integer> result = new HashMap<>();
+        List<OrderItem> orderItems = orders.stream().flatMap(o -> o.getOrderItems().stream()).collect(Collectors.toList());
+        for (OrderItem oi : orderItems) {
+            if (result.containsKey(oi.getProduct())) {
                 result.put(oi.getProduct(), result.get(oi.getProduct()) + oi.getQuantity());
-            }else{
+            } else {
                 result.put(oi.getProduct(), oi.getQuantity());
             }
         }
@@ -114,5 +113,5 @@ public class OrderCommonsBean extends AbstractCommonsBean<UserOrder> implements 
         }
         return result;
     }
-    
+
 }
